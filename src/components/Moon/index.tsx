94d5b3e71b0a114f1b41afa5b582b921b3c82247
André Carvalho@note <andre.vrlcarvalho@hotmail.com>
2024-01-17 11:00:04 +0000

    moon phases

--- src/components/Moon/index.tsx
+++ src/components/Moon/index.tsx
@@ -1,11 +1,22 @@
 // Moon.js
 import React from 'react';
 import * as Styled from './styles';
+import { Moon as MoonPhase } from 'lunarphase-js';
+
+const getAgeString = (age: number): string => {
+  if (age < 1.84566) return 'New';
+  else if (age < 12.91963) return 'First Quarter';
+  else if (age < 16.61096) return 'Full';
+  else if (age < 27.68493) return 'Last Quarter';
+  return 'New';
+};
 
 export const Moon = () => {
+  const phase = getAgeString(MoonPhase.lunarAge());
   return (
     <Styled.Wrapper>
       <div></div>
+      <Styled.Phase phase="Waxing Gibbous"></Styled.Phase>
     </Styled.Wrapper>
   );
 };
--- src/components/Moon/styles.ts
+++ src/components/Moon/styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 const spinAnimation = keyframes`
   to {
@@ -27,7 +27,7 @@ export const Wrapper = styled.div`
     left: 0;
     border-color: #000;
     border-radius: 50%;
-    box-shadow: -40px -40px 50px 2px #000 inset;
+    box-shadow: -10px -10px 50px 2px #000 inset;
   }
 
   > div {
@@ -36,4 +36,50 @@ export const Wrapper = styled.div`
     animation: ${spinAnimation} 40s linear alternate infinite;
     background: url('/assets/images/moon.jpg');
   }
+`;
+
+const getTopRadius = (moonStage) => {
+  switch (moonStage) {
+    case 'Full':
+      return '150%';
+    case 'First Quarter':
+      return '0%';
+    case 'New':
+      return '1%';
+    case 'Last Quarter':
+      return '0%';
+    default:
+      return '0%';
+  }
+};
+
+const getLeftRadius = (moonStage) => {
+  switch (moonStage) {
+    case 'Full':
+      return '100%';
+    case 'First Quarter':
+      return '20%';
+    case 'New':
+      return '1%';
+    case 'Last Quarter':
+      return '-20%';
+    default:
+      return '5%';
+  }
+};
+
+export const Phase = styled.section<{ phase: string }>`
+  ${({ theme, phase }) => css`
+  width: 390px;
+  height: 390px;
+  position: absolute;
+  top: ${getTopRadius(phase)};
+  left: ${getLeftRadius(phase)};
+  margin: auto;
+  overflow: hidden;
+  border-radius: 50%;
+  z-index: 3;
+  background-color: black;
+  opacity: .87;
+  `}
 `;
